c222c2b171bf70e6a2f17077d54ee3baca93f712
alfi sahri <alfisahri33@gmail.com>
2020-12-17 07:09:45 +0700

    refactor

--- app.js
+++ app.js
@@ -2,17 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-
-const isAuth = require("./middlewares/isAuth");
 
 const app = express();
 const port = 5000;
-
-// model
-const User = require("./models/User");
-const { token } = require("morgan");
 
 // connect db
 mongoose
@@ -38,83 +30,3 @@ app.get("/", (req, res) => {
 
 const usersRoute = require("./routes/usersRoute");
 app.use("/api/users", usersRoute);
-
-// // auto login
-// app.get("/auto-login", isAuth, (req, res) => {
-//   User.findById(req.user.id)
-//     .select("-password")
-//     .then((doc) => {
-//       res.json(doc);
-//     });
-// });
-
-// // login
-// app.post("/login", (req, res) => {
-//   const { email, password } = req.body;
-
-//   console.log("==>", req.body);
-
-//   if (!email || !password) {
-//     return res.status(400).json({ msg: "failed!" });
-//   }
-
-//   User.findOne({ email: email }).then((doc) => {
-//     if (!doc)
-//       return res.status(400).json({ msg: "email or password is invalid" });
-
-//     console.log(`doc => ${doc}| password : ${password}`);
-//     bcrypt.compare(password, doc.password).then((isPasswordMatch) => {
-//       if (isPasswordMatch) {
-//         jwt.sign({ id: doc._id }, "rahasia", (err, token) => {
-//           if (err) throw err;
-//           res.json({
-//             msg: "login success ",
-//             user: { email: doc.email, id: doc._id },
-//             token,
-//           });
-//         });
-//       } else {
-//         res.status(400).json({ msg: "email or password is invalid" });
-//       }
-//     });
-//   });
-// });
-
-// // register
-// app.post("/register", (req, res) => {
-//   const { email, password } = req.body;
-
-//   console.log("==>", req.body);
-
-//   if (!email || !password) {
-//     return res.status(400).json({ msg: "failed!" });
-//   }
-
-//   bcrypt.genSalt(10, (err, salt) => {
-//     if (err) throw err;
-
-//     // cek user
-//     User.findOne({ email: email }).then((doc) => {
-//       // cek jika email udah terdaftar
-//       if (doc)
-//         return res.status(400).json({ message: "email is already exist" });
-
-//       // hash password
-//       bcrypt.hash(password, salt, (err, passHash) => {
-//         if (err) throw err;
-
-//         new User({
-//           email: email,
-//           password: passHash,
-//         })
-//           .save()
-//           .then((doc) => {
-//             jwt.sign({ id: doc._id }, "rahasia", (err, token) => {
-//               res.json({ message: "register success ", id: doc._id, token });
-//             });
-//           })
-//           .catch((err) => console.log(err));
-//       });
-//     });
-//   });
-// });
